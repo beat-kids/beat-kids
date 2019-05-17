539a5d18fd9f9fbe8f302ec5ff7d3b77555de51d
baemincheon <baemincheon@gmail.com>
2019-05-18 01:30:58 +0900

    import asset & make basic beat button implementation

---
+++ beat-kids/Assets/Resources/Scripts/BeatButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BeatButton : MonoBehaviour
+{
+    public Sprite mSpriteNormal = null;
+    public Sprite mSpritePressed = null;
+    public UnityEvent mFunction = null;
+
+    private SpriteRenderer mSpriteRenderer = null;
+
+    public void PointEnter()
+    {
+        this.mFunction.Invoke();
+
+        this.ChangeSpriteAsync();
+    }
+
+    private void Awake()
+    {
+        this.mSpriteRenderer = this.GetComponent<SpriteRenderer>();
+    }
+
+    private async void ChangeSpriteAsync()
+    {
+        this.mSpriteRenderer.sprite = this.mSpritePressed;
+
+        await Task.Delay(500);
+
+        this.mSpriteRenderer.sprite = this.mSpriteNormal;
+    }
+}
